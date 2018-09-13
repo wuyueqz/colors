a7b8f24fd28da81a2c03ef6fafc3d312c06c767b
wuyueqz <wuyueqz@gmail.com>
2018-09-13 15:28:39 -0400

    Modified main.js

--- main.js
+++ main.js
@@ -2,3 +2,20 @@
 //----------------------------------
 //          YOUR CODE
 //----------------------------------
+var counter = 0;
+
+for (var j = 0; j < 81; j ++) {
+  
+  y = j*15
+
+  for (var i = 0; i < 53; i++) {
+
+    x = i*15
+
+    create(x,y,colors[counter]);
+
+    counter++;
+
+    }
+
+}
